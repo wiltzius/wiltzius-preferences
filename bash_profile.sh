30510e6f03a13d276b104b08260ee1b663c5f265
Tom Wiltzius <wiltzius@chromium.org>
2014-09-30 23:05:20 -0700

    updated bash profile, adding git prompt

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,10 +1,23 @@
 # Tom Wiltzius bash aliases
 
-alias ll='ls -aGlh'
+# Environment variables
 export PATH=~/Documents/sdk/platform-tools/:$PATH
 export PATH=~/Documents/chromium/depot_tools/:$PATH
-alias canary='/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --enable-impl-side-painting --enable-skia-benchmarking &'
+export CLICOLOR=1
+export GREP_OPTIONS='--color=auto'    # Tell grep to highlight matches
 
+# Bind key events from file
 bind -f ~/wiltzius-preferences/inputrc.sh
 
+# Useful aliases
 alias cowville='ssh -p 23 tom@cowville.net'
+alias canary='/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --enable-impl-side-painting --enable-skia-benchmarking &'
+alias ll='ls -aGlh'
+
+# Set prompt
+source ~/wiltzius-preferences/git-prompt.sh       # Load in the git branch prompt script.
+GREEN='\e[0;32m'
+MAGENTA='\[\033[0;35m\]'
+WHITE='\[\033[m\]'
+PS1="\w\[$MAGENTA\]\$(__git_ps1)\[$GREEN\] \$ $WHITE"
+
