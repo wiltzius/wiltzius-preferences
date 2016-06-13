01b82ce880a75bdef5b98ed5436df5a47e5c20fe
Tom Wiltzius <tom.wiltzius@gmail.com>
2016-06-13 14:01:47 -0700

    welkin -> w

--- bash_profile.sh
+++ bash_profile.sh
@@ -21,7 +21,7 @@ export WELKIN_ROOT=$HOME/Documents/welkin
 export WELKIN_VIRTUALENV_PATH=$HOME/Documents/ENV
 alias envactivate='source $WELKIN_VIRTUALENV_PATH/bin/activate'
 alias enter_welkin_env='cd $WELKIN_ROOT; envactivate'
-alias welkin=$WELKIN_ROOT/welkin
+alias w=$WELKIN_ROOT/welkin
 envactivate
 ssh-add ~/.ssh/welkin-new-servers.pem &> /dev/null
 
